74cd7d213217643acbbdb076c5dd1003d82c87a1
sdhushu <1426732052@qq.com>
2022-11-19 14:40:16 +0800

    feat: workflow page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,23 +2,21 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: '每天学习打卡',
+  base: '/date_learn/',
   description: '当你焦虑的时候，行动起来！',
   themeConfig: {
-    logo: '/logo.jpeg',
+    logo: '/book.jpeg',
     siteTitle: '每天学习打卡',
     socialLinks: [
       { icon: 'github', link: 'https://github.com/sdhushu' }
     ],
-    nav: [
-      { text: '首页', link: '/index' }
-    ],
     sidebar: [
       {
-        text: '学习笔记',
+        text: '跟着大佬学知识',
         collapsible: true,
         collapsed: true,
         items: [
-          { text: '测试', link: '/guide/'}
+          { text: 'day01', link: '/guide/'}
         ]
       }
     ]
